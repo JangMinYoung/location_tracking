675be4218cc016afc22943966989254adcf3e88a
JangMinYoung <min8540055@hanmail.net>
2018-07-21 03:09:01 +0900

    페이스북 로그인(완성)

--- location_tracking/app/src/main/java/com/example/minyoung/location_tracking/LoginActivity.java
+++ location_tracking/app/src/main/java/com/example/minyoung/location_tracking/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.minyoung.location_tracking;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -9,35 +8,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
-import com.kakao.auth.AuthType;
-import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
-import com.kakao.network.ErrorResult;
-import com.kakao.usermgmt.LoginButton;
-import com.kakao.usermgmt.UserManagement;
-import com.kakao.usermgmt.callback.MeResponseCallback;
-import com.kakao.usermgmt.response.model.UserProfile;
-import com.kakao.util.exception.KakaoException;
-import com.kakao.util.helper.log.Logger;
+import com.facebook.login.widget.LoginButton;
+//import com.kakao.usermgmt.LoginButton;
+
 
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button login;
-    private Button singup;
+//    private Button singup;
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
     //카카오톡 로그인을 위한 변수
     private Button btn_custom_login;
-    private LoginButton btn_kakao_login;
+    private com.kakao.usermgmt.LoginButton btn_kakao_login;
     private SessionCallback callback;
+    //페이스북 로그인을 위한 변수
+    private static final String TAG = "LoginActivity";
+    private CallbackManager callbackManager;
+    private com.facebook.login.widget.LoginButton btn_facebook_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
         mFirebaseRemoteConfig=FirebaseRemoteConfig.getInstance();
         String splash_background=mFirebaseRemoteConfig.getString("splash_background");
 
@@ -54,25 +55,42 @@ public class LoginActivity extends AppCompatActivity {
                 btn_kakao_login.performClick();
             }
         });
-        btn_kakao_login = (LoginButton) findViewById(R.id.btn_kakao_login);
+        btn_kakao_login = (com.kakao.usermgmt.LoginButton) findViewById(R.id.btn_kakao_login);
 
-        login=(Button)findViewById(R.id.loginActivity_button_login);
-        singup =(Button)findViewById(R.id.loginActivity_button_signup);
-        login.setBackgroundColor(Color.parseColor(splash_background));
-        singup.setBackgroundColor(Color.parseColor(splash_background));
-        singup.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
-            }
-        });
-        login.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                startActivity(new Intent(LoginActivity.this,MainActivity.class));
-            }
-        });
+        //페이스북 연동
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
+        btn_facebook_login = findViewById(R.id.btn_facebook_login);
 
+        btn_facebook_login.setReadPermissions("email");
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        Log.d(TAG, "onSucces LoginResult=" + loginResult);
+                        Intent intent =new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        Log.d(TAG, "onCancel");
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        Log.d(TAG, "onError");
+                    }
+                });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 
